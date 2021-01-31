eb657af34dd0857fccac5b3713c446a210288834
Magda Barthofer <magda.barthofer@gmail.com>
2021-01-31 21:05:47 +0100

    chached comments on coding challenges, refactor coding challenges

--- best_travel.rb
+++ best_travel.rb
@@ -1,33 +1,18 @@
 def choose_best_sum(t, k, ls)
-  # t is limit of km to be driven
-  # k is number of towns to be visitd
-  # ls is array of distances between towns
-  # Setting variable i to be 0
-  i = 0
-  # Creating an empty array called array
   array = []
-  # Loop through all the combinations of sums of distances
-  # with k as the number of towns to be visited therefor the number of digits of the combination
-  # and saving those in the array
-  while i < ls.count
+  # finding all sums of combinations
+  ls.count.times do
     array << ls.combination(k).to_a.map(&:sum)
-    i += 1
   end
-  # calling method
-  filter_or_find_next_smallest(array, t)
+  find_next_biggest(array, t)
 end
 
-def filter_or_find_next_smallest(array, t)
-  # Return nil if max of array is nil
-  if array.flatten.max.nil?
-    nil
-  # Return max of array if smaller than t, therefor finding the max of km if smaller than t
-  elsif array.flatten.max < t
-    array.flatten.max
-  # Return t if t exists (index is not nil)
-  elsif !array.flatten.find_index(t).nil?
+def find_next_biggest(array, t)
+  array.flatten.reject!(&:nil?)
+  # Return t if t is part of array
+  if array.flatten.include? t
     t
-  # Else return value (x) of array that is the next smallest to t
+  # Return the next biggest value of t
   else array.flatten.sort.reverse.bsearch { |x| x <= t }
   end
 end
--- next_bigger.rb
+++ next_bigger.rb
@@ -6,31 +6,33 @@ class Array
   end
 end
 
+# special case 1: there is no bigger number, because array is already in descending order
+def special_case_1?(array)
+  array.sort.reverse == array
+end
+
+# special case 2: array is in ascending order
+def special_case_2?(array)
+  array.sort == array && array.uniq.count == array.count
+end
+
+# special case 3: array has 3 values or less, second last and last value are the same
+def special_case_3?(array)
+  array.count <= 3 && array[-1] == array[-2]
+end
+
 def swap_special_case(array)
-  # special case: there is no bigger number,
-  # return -1
-  return -1 if array.sort.reverse == array
-  # special case: array is in ascending order and there are no doublicate numbers,
-  # swap second last and last value, use return to stop program
-  return array.swap!(-1, -2).join.to_i if array.sort == array && array.uniq.count == array.count
-  # special case: array has 3 values or less, second last and last value are the same
-  # swap third and second last value, use return to stop program
-  return array.swap!(-2, -3).join.to_i if array[-1] == array[-2] && array.count <= 3
+  return -1 if special_case_1?(array)
+  return array.swap!(-1, -2).join.to_i if special_case_2?(array)
+  return array.swap!(-2, -3).join.to_i if special_case_3?(array)
 end
 
 def swap_not_special_case(array)
   i = 1
   while i < array.count
-    # look from right side for the first value that is bigger than the previous one (left side)
     if array[-i] > array[-i - 1]
-      # range from -i till the last value, save range as until_last, sort it
-      until_last = array[-i.. - 1].sort
-      # find index of value to be switched
-      to_switch = (until_last.find_index(until_last.detect { |x| x > array[-1-i] }) - until_last.count)
-      # return range of array from left side beginning until value before -1
-      # instert until_last in the previous
-      # flatten this array and switch
-      # return integer
+      until_last = array[-i..-1].sort
+      to_switch = (until_last.find_index(until_last.detect { |x| x > array[-1 - i] }) - until_last.count)
       return array[0..-i - 1].push(until_last).flatten.swap!(-i - 1, to_switch).join.to_i
     end
     i += 1
@@ -38,14 +40,9 @@ def swap_not_special_case(array)
 end
 
 def next_bigger(n)
-  # transform integer to array of digits, store in variable array
   array = n.to_s.chars.map(&:to_i)
-  # perform swap if special case
-  if array.sort.reverse == array ||
-    array.sort == array && array.uniq.count == array.count ||
-    array[-1] == array[-2] && array.count <= 3
+  if special_case_1?(array) || special_case_2?(array) || special_case_3?(array)
     swap_special_case(array)
-    # perform swap if not special case
   else swap_not_special_case(array)
   end
 end
